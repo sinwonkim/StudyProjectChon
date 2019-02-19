bed735b35e2a75691b280e5e4b2535594dfd1ac1
sinwonkim <zhfps114@gmail.com>
2019-02-19 20:57:04 +0900

    String 클래스와 ==참조연산자

--- javapractice/src/stringClassEx/StringClassEx.java
+++ javapractice/src/stringClassEx/StringClassEx.java
@@ -7,13 +7,28 @@ public class StringClassEx {
 	}
 	
 	public static void main(String[] args) {
-		String str1 = new String("Simple String");
-		String str2 = "Test string";
+		String str1 = new String("Simple String"); // String 인스턴스 생성방법 1
+		String str3 = new String("Simple String"); // String 인스턴스 생성방법 1
+		
+		if(str1 == str3) { // ==은 참조 변수를 대상으로 한 '참조변수의 참조 값'에 대한 비교 연산을 진행한다.
+			System.out.println("str1과 str3은 동일 인스턴스 참조 ");
+		}else
+			System.out.println("str1과 str3은 동일 인스턴스 참조하지 않는다.");
+	
+		String str2 = "Test string"; // String 인스턴스 생성방법 2
+		String str4 = "Test string"; // String 인스턴스 생성방법 2
+		
+		if(str2 == str4) {
+			System.out.println("str2와 str4는 동일 인스턴스를 참조한다.");
+		}else
+			System.out.println("str2와 4는 동일 인스턴스를 참조하지 않는다.");
 		
 		System.out.println(str1);
 		System.out.println(str2.length());
 		
-		showString("adios");
+		showString("adios"); // adios라는  String 인스턴스가 만들어진다. 
+		
+		
 	}
 
 }
