7de94ccd2c8b4f33da2aef6da358fc53231859cf
sinwonkim <zhfps114@gmail.com>
2019-02-20 20:18:02 +0900

    #Enhanced for문 ex1

---
+++ javapractice/src/enhancedFor/EnhancedFor.java
@@ -0,0 +1,12 @@
+package enhancedFor;
+
+public class EnhancedFor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] ar = {1,2,3,4,5};
+		for(int e : ar) {
+			System.out.println(e);
+		}
+	}
+}
